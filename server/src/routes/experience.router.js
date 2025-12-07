dee0de4bf706a05a8fb7eb7da197696e86413b6f
luisa <moraLF20@outlook.com>
2025-12-06 21:41:37 -0700

    agregando autenticacion a rutas de experiencia

--- server/src/routes/experience.router.js
+++ server/src/routes/experience.router.js
@@ -1,12 +1,13 @@
 import e from 'express';
 import { addExperience, deleteExperience, getAll, getExperience, updateExperience } from '../controllers/experience.controller.js';
 import { validateExperience, validateExperienceId } from '../middlewares/validators/experienceValidator.js';
+import { authenticate, authorize } from '../middlewares/authMiddleware.js'
 const experienceRouter = e.Router();
 
 experienceRouter.get("/",getAll);
 experienceRouter.get("/:id",validateExperienceId(), getExperience);
-experienceRouter.post("/", validateExperience(),addExperience);
-experienceRouter.put("/:id",validateExperienceId(),validateExperience(),updateExperience);
-experienceRouter.delete("/:id",validateExperienceId(),deleteExperience);
+experienceRouter.post("/", authenticate, authorize(['proveedor']), validateExperience(),addExperience);
+experienceRouter.put("/:id",authenticate, authorize(['proveedor']),validateExperienceId(),validateExperience(),updateExperience);
+experienceRouter.delete("/:id",authenticate, authorize(['proveedor']),validateExperienceId(),deleteExperience);
 
 export default experienceRouter;
